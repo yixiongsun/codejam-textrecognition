2e8b5f7f6b8245576d33dd2e1fba4ae2f0d4068e
Yixiong Sun <yixiongsun@Maxs-MacBook-Pro.local>
2018-11-17 10:45:31 -0500

    Base64 request

--- libraries/googleapi.js
+++ libraries/googleapi.js
@@ -18,20 +18,28 @@ module.exports = class GoogleAPI {
     }
 
     textDetection() {
-        return new Promise(function(reject, resolve) {
-            client
-            .textDetection('./resources/wakeupcat.jpg')
-            .then(results => {
-                const labels = results[0].textAnnotations;
+        return new Promise(function (reject, resolve) {
+            var fs = require('fs');
+            var imageFile = fs.readFileSync('./resources/wakeupcat.jpg');
 
-                console.log(labels);
-                labels.forEach(label => console.log(label.description));
-                resolve(labels)
-            })
-            .catch(err => {
-                console.error('ERROR:', err);
-                reject(err)
-            });
+            // Convert the image data to a Buffer and base64 encode it.
+            var encoded = Buffer.from(imageFile).toString('base64');
+            let request = {
+                image: { content: encoded }
+            };
+            client
+                .textDetection(request)
+                .then(results => {
+                    const labels = results[0].textAnnotations;
+
+                    console.log(labels);
+                    labels.forEach(label => console.log(label.description));
+                    resolve(labels)
+                })
+                .catch(err => {
+                    console.error('ERROR:', err);
+                    reject(err)
+                });
         })
     }
 }
